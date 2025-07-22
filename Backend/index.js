241b0759bcf1c9c58474b5f679e8e6b9062769c8
NisnahtSingh <nishantsinghbhati96@gmail.com>
2025-07-22 23:46:31 +0530

    dfs

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ connectDB();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
-
+app.use(fileUpload());
 const allowedOrigins = [
     'https://www.directhire.in' ,
     'https://directhire.in',
